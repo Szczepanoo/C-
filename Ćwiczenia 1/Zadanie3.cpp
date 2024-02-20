3a3471b2e83df17cc05f10698ffac378b2092512
Jakub <125917209+Szczepanoo@users.noreply.github.com>
2024-02-20 12:47:35 +0100

    Update Zadanie3.cpp

--- Ćwiczenia 1/Zadanie3.cpp
+++ Ćwiczenia 1/Zadanie3.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+int main()
+{
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    for (int i = 0; i <= 9; i++) {
+        int silnia = 1;
+        for (int j = array[i]; j >= 1; j--) {
+
+            silnia = silnia * j;
+
+        }
+        printf("%d\n", silnia);
+    }
+
+    return 0;
+
+
+}
