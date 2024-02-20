be1ec1323f1000f141d02234c4244ceac1c6e0db
Jakub <125917209+Szczepanoo@users.noreply.github.com>
2024-02-20 12:36:11 +0100

    Update Zadanie2.cpp

--- Ćwiczenia 1/Zadanie2.cpp
+++ Ćwiczenia 1/Zadanie2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+int main()
+{
+    int grades[5][2] = { {3,4},{5,4},{2,5},{3,3},{4,2} };
+
+    double suma_oceny_matematyka = 0;
+    double suma_oceny_fizyka = 0;
+
+    for (int i = 0; i <= 4; i++)
+    {
+        suma_oceny_matematyka += grades[i][0];
+    }
+
+
+    for (int i = 0; i <= 4; i++)
+    {
+        suma_oceny_fizyka += grades[i][1];
+    }
+
+    printf("Srednia ocen z matematyki: %.2f \n", suma_oceny_matematyka / 5);
+    printf("Srednia ocen z matematyki: %.2f", suma_oceny_fizyka / 5);
+
+    return 0;
+}
