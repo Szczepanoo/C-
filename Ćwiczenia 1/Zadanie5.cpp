d23f42aa2c874779dc60340baaa23c356de8601b
Jakub <125917209+Szczepanoo@users.noreply.github.com>
2024-02-20 13:38:27 +0100

    Update Zadanie5.cpp

--- Ćwiczenia 1/Zadanie5.cpp
+++ Ćwiczenia 1/Zadanie5.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+int main()
+{
+    int n, suma = 0;
+
+    printf("Podaj liczbe:");
+    scanf_s("%d",&n);
+    if (n < 0) {
+        printf("Suma: 0");
+        return 0;
+    }
+
+    
+    for (int i = 1; i < n; i++) {
+        if (i % 3 == 0) {
+            suma += i;
+            printf("%d \n", i);
+        }
+
+        else if (i % 5 == 0) {
+            suma += i;
+            printf("%d \n", i);
+        }
+    }
+
+    printf("Suma: %d", suma);
+
+
+    return 0;
+}
