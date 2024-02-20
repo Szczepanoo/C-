b9bf9c4ea379cd79ce98e349585690b410a5db49
Jakub <125917209+Szczepanoo@users.noreply.github.com>
2024-02-20 11:56:49 +0100

    Update Zadanie1.cpp

--- Ćwiczenia 1/Zadanie1.cpp
+++ Ćwiczenia 1/Zadanie1.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    char imie[50];
+
+    printf("Podaj swoje imie: ");
+    scanf_s("%s", imie, sizeof(imie));
+
+    printf("Witaj, %s\n", imie);
+
+    return 0;
+}
